298699ec4916da61b690bd181784af0938ffdc78
spideryzarc <spideryzarc@gmail.com>
2025-04-14 12:54:47 -0300

    feat: Add geo.py for geolocation functionalities and remove sqlite.py

---
+++ codes/router/geo.py
@@ -0,0 +1,5 @@
+https://osmnx.readthedocs.io/en/stable/
+#TODO: dado um endereço, retorna a latitude e longitude
+#TODO: dado duas coordenadas, retorna a distância entre elas (na malha viária)
+#TODO: dado uma poligono, ou uma localidade, plotar um mapa.
+#TODO: dado duas coordenadas, plotar o caminho entre elas ( rua a rua)
