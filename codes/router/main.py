57d4dbd2cf1635395689828b22e7374859c2ad3c
spideryzarc <spideryzarc@gmail.com>
2025-05-07 21:10:01 -0300

    pydantic_sqlalchemy

--- codes/router/main.py
+++ codes/router/main.py
@@ -1,12 +1,24 @@
 from model import Session, Depots
 from typing import Union
 
-import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
 import osmnx as ox
+import uvicorn
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
 app = FastAPI()
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+        db.commit()
+    except:
+        db.rollback()
+        raise
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -18,39 +30,23 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-class Item(BaseModel):
-    name: str
-    age: int
 
-@app.get("/echo")
-def echo(x:Item):
-    print("echo data:\n", x)
-    return {"received_data": x}
+DepotsCreateSchema = sqlalchemy_to_pydantic(Depots, exclude=["id", "latitude", "longitude"])
 
-class ValidDepot(BaseModel):
-    name: str
-    adress: str
-    latitude: Union[float, None] = None
-    longitude: Union[float, None] = None
 
 @app.get("/add_depot")
-def add_depot(depot: ValidDepot):
+def add_depot(depot: DepotsCreateSchema, db: Session = Depends(get_db)):
     print("depot data:\n", depot)
+    # search coordinates  if necessary
     if not depot.latitude or not depot.longitude:
-        # search for depot coordinates
-        depot.latitude, depot.longitude = ox.geocode(depot.adress)
-    with Session() as session:
-        depot_data = Depots(
-            name=depot.name,
-            address=depot.adress,
-            latitude=depot.latitude,
-            longitude=depot.longitude
-        )
-        session.add(depot_data)
-        session.commit()
-        session.refresh(depot_data)
-    print("Depot added to DB:\n", depot_data)
-    return {"status": "success", "data": depot_data}     
+        depot.latitude, depot.longitude = ox.geocode(depot.address)
+    new_depot = Depots(**depot.dict())
+    db.add(new_depot)
+    db.commit()
+    db.refresh(new_depot)
+    print("Depot added to DB:\n", new_depot)
+    return {"status": "success", "new_id": new_depot.id}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host="0.0.0.0")
